f232aa5de6d8981750c81289e92e376dc487ddc8
Jiacheng Huang <huangjiacheng0709@outlook.com>
2021-10-22 02:18:55 -0500

    Separate installation to install.sh, configure.sh, and post_install.sh

---
+++ configure.sh
@@ -0,0 +1,60 @@
+# Configure the system
+
+## Time zone
+echo "Setting the time zone..."
+ln -sf /usr/share/zoneinfo/$REGION/$CITY /etc/localtime
+echo "Running hwclock to generate /etc/adjtime..."
+hwclock --systohc
+echo "This command assumes the hardware clock is set to UTC."
+
+## Localization
+cp ./etc/locale.gen /etc/locale.gen
+echo "Generating the locales..."
+locale-gen
+echo "Setting the LANG variable..."
+cp ./etc/locale.conf /etc/locale.conf
+
+if [ ! -z $KEYMAP ]
+then
+    echo "Making the console keyboard layout persistent..."
+    echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+fi
+
+## Network configuration
+echo "Creating the hostname file..."
+echo $HOSTNAME > /etc/hostname
+echo "Matching entries to hosts..."
+echo "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.0.1\t$HOSTNAME" > /etc/hosts
+
+## Initramfs
+### Creating a new initramfs is usually not required, because mkinitcpio was run on installation of the kernel package with pacstrap.
+### For LVM, system encryption or RAID, modify mkinitcpio.conf(5), uncomment the following command and recreate the initramfs image:
+### mkinitcpio -P
+
+## Root password
+echo "Setting the root password..."
+passwd
+
+## Boot loader
+echo "Installing GRUB..."
+pacman -S grub efibootmgr
+if [ ! -d /efi ]
+then
+    mkdir /efi
+fi
+grub-install --target=x86_64-efi --efi-directory=efi --bootloader-id=GRUB
+echo "Generating `/boot/grub/grub.cfg`..."
+grub-mkconfig -o /boot/grub/grub.cfg
+echo "Enabling microcode updates (if you have an Intel or AMD CPU)..."
+if [ $CPU_MANUFACTURER = "amd" ]
+then
+    pacman -S amd-ucode
+fi
+if [ $CPU_MANUFACTURER = "intel" ]
+then
+    pacman -S intel-ucode
+fi
+
+# Switch user
+echo "Switching to 'USERNAME'..."
+su - $USERNAME
---
+++ post_install.sh
@@ -0,0 +1,85 @@
+# Post-installation
+
+## Network manager
+echo "Installing network manager '$NETWORK_MANAGER'..."
+sudo pacman -S $NETWORK_MANAGER
+echo "Enabling '$NETWORK_MANAGER'..."
+systemctl enable $NETWORK_MANAGER_SERVICE
+
+## Text editor
+echo "Installing text editor '$TEXT_EDITOR'..."
+sudo pacman -S $TEXT_EDITOR
+
+## Documentation tools
+echo "Installing packages for accessing documentation in `man` and `info` pages: `$DOCUMENTATION_TOOLS`..."
+sudo pacman -S $DOCUMENTATION_TOOLS
+
+## Paru
+echo "Installing paru..."
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+## Zsh
+echo "Installing zsh..."
+sudo pacman -S zsh zsh-completions
+echo "Changing default shell to zsh..."
+chsh -s /usr/bin/zsh
+echo "Installing Oh My Zsh..."
+paru -S oh-my-zsh-git
+
+## Users and groups
+echo "Creating user '$USERNAME'..."
+useradd -m $USERNAME -s /usr/bin/zsh
+
+## Reflector
+echo "Installing Reflector..."
+sudo pacman -S reflector
+echo "Enabling Reflector..."
+systemctl enable reflector.service
+
+## Xorg
+echo "Installing Xorg..."
+sudo pacman -S xorg
+
+## Drivers
+echo "Installing Drivers..."
+if [ $GPU_MANUFACTURER = "amd" ]
+then
+    sudo pacman -S xf86-video-amdgpu
+fi
+if [ $GPU_MANUFACTURER = "intel" ]
+then
+    sudo pacman -S xf86-video-intel
+fi
+if [ $GPU_MANUFACTURER = "nvidia" ]
+then
+    sudo pacman -S nvidia
+fi
+
+## KDE
+echo "Installing Plasma and KDE applications..."
+sudo pacman -S plasma-meta kde-applications-meta
+
+## SDDM
+echo "Enabling SDDM..."
+systemctl enable sddm.service
+
+## Fonts
+echo "Installing Chinese fonts..."
+sudo pacman -S wqy-microhei wqy-microhei-lite wqy-bitmapfont wqy-zenhei ttf-arphic-ukai ttf-arphic-uming adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts noto-fonts-cjk
+echo "Installing Microsoft fonts..."
+sudo pacman -S ttf-ms-fonts
+
+## Browser
+echo "Installing '$BROWSER'..."
+sudo pacman -S $BROWSER
+
+## Email client
+echo "Installing '$EMAIL_CLIENT'..."
+sudo pacman -S $EMAIL_CLIENT
+
+# Reboot
+## Optionally manually unmount all the partitions with umount -R /mnt: this allows noticing any "busy" partitions, and finding the cause with fuser.
+echo "Rebooting..."
+reboot
